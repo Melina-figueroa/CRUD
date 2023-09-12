381e2095812b26805a7d68cda9a09e30e2fca5cb
Melina-figueroa <melinafigueroa@tdf.edu.ar>
2023-09-12 15:14:00 -0300

    se agrega orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CiudadModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'tino3719',
+    database: 'db_colegio',
+    entities: [__dirname + '/**/**/**.entity{.ts,.js}'],
+    synchronize: true //solo se usa en modo desarrollador en produccion tenemos acceso a la bd de produccion
+  }),
+  CiudadModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
